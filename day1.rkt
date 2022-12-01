2bdb4d66f3172eb36ee909dd0a7d401344d9529a
Arthur Mendes <arthurmco@gmail.com>
2022-12-01 04:18:05 -0300

    day 1 part 1

---
+++ day1.rkt
@@ -0,0 +1,42 @@
+#lang racket
+
+
+(define (read-calories-line cfile)
+  (let ([cline (read-line cfile)])
+    (if (eof-object? cline)
+        '()
+        (cons cline (read-calories-line cfile)))))
+
+(define (open-calories-file filename)
+  (call-with-input-file filename
+    (lambda (cfile)
+      (read-calories-line cfile))))
+
+
+(define (split-calories-by-elf raw-calories)
+  (list->vector
+   (foldl (lambda (value calories-list)
+            (if (equal? (string-trim value) "")
+                (append calories-list (list '()))
+                (let ([current-elf (last calories-list)]
+                      [calories-before (drop-right calories-list 1)])
+                  (append calories-before (list (append current-elf (list (string->number value))))))))
+          (list '()) raw-calories)))
+
+
+
+(define (sum-calories-per-elf calories-vec)
+  (vector-map (lambda (v) (foldl + 0 v)) calories-vec))
+
+(define (get-biggest-calorie-elf calories-vec)
+  (let ([max-value (foldl max 0 (vector->list calories-vec))])
+    (vector-member max-value calories-vec)))
+
+(define (run-script filename)
+  (let* ([calories (split-calories-by-elf (open-calories-file filename))]
+         [calories-total (sum-calories-per-elf calories)])
+    (printf "Elf with most calories is ~A, with ~A\n"
+            (+ 1 (get-biggest-calorie-elf calories-total))
+            (vector-ref calories-total (get-biggest-calorie-elf calories-total)))))
+  
+
