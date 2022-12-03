dcb341a95aa5dde9d2434c49b6efe3922c51981b
Arthur Mendes <arthurmco@gmail.com>
2022-12-03 13:01:57 -0300

    day 3 part 2

--- day3.rkt
+++ day3.rkt
@@ -16,11 +16,21 @@
       (read-rucksack-line cfile))))
 
 
+(define (split-rucksacks-by-elf-groups sacks)
+  (if (null? sacks)
+      '()
+      (let-values ([(group rest) (split-at sacks 3)])
+        (cons group (split-rucksacks-by-elf-groups rest)))))
+
 (define (get-common-item-type-on-compartments sacks)
   (set-first (apply set-intersect
                     (map (compose list->set string->list)
                          sacks))))
 
+
+(define (get-common-item-type-on-group sack-group)
+  (get-common-item-type-on-compartments
+   (map (lambda (r) (apply string-append r)) sack-group)))
 
 
 (define (get-item-priority item)
@@ -36,3 +46,10 @@
          [common-items (map get-common-item-type-on-compartments rucksacks)]
          [priorities (map get-item-priority common-items)])
     (printf "Sum of priorities: ~A\n" (apply + priorities))))
+
+(define (run-script-2 filename)
+  (let* ([rucksacks (open-rucksack-file filename)]
+         [groups (split-rucksacks-by-elf-groups rucksacks)]
+         [common-items (map get-common-item-type-on-group groups)]
+         [priorities (map get-item-priority common-items)])
+    (printf "Sum of priorities for all groups: ~A\n" (apply + priorities))))
