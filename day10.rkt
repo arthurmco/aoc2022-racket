242a5edd22bc8d7b8e8e24b0ce887d73b4edb8be
Arthur Mendes <arthurmco@gmail.com>
2022-12-10 12:28:36 -0300

    day 10 part 2

--- day10.rkt
+++ day10.rkt
@@ -28,33 +28,84 @@
       ['noop registers]
       ['addx (regs (+ (regs-x registers) (cdr instruction)))]))
 
+  (define (sprite-region registers)
+    (list (- (regs-x registers) 1) (regs-x registers) (+ (regs-x registers) 1)))
+  
+  (define (update-screen screen sprite-index cycle-num registers)
+    (if (or (eq? screen #f) (>= cycle-num (vector-length screen)))
+        screen               
+        (vector-map (lambda (pixel index)
+                      (let ([probable-sprite (map (curry max 0) (sprite-region registers))])
+                        (if (and (= index cycle-num)
+                                 (list? (member (remainder index 40) probable-sprite)))
+                              #t
+                              pixel)))
+                    screen
+                    (list->vector (range (vector-length screen))))))
 
-  (define (execute-for-cycles instruction-cycle-map registers cycle-count)
-    (if (or (= (length instruction-cycle-map) 0) (= cycle-count 0))
-        (cons instruction-cycle-map registers)
-        (let* ([instruction-cycle-item (car instruction-cycle-map)]
-               [remaining-inst-cycles (vector-ref instruction-cycle-item 0)]
-               [instruction (vector-ref instruction-cycle-item 1)])
-          (if (> remaining-inst-cycles 1)
-              (execute-for-cycles
-               (cons
-                (vector (- remaining-inst-cycles 1) instruction)
-                (cdr instruction-cycle-map))
-               registers
-               (- cycle-count 1))
-              (execute-for-cycles
-               (cdr instruction-cycle-map)
-               (execute-instruction instruction registers)
-               (- cycle-count 1))))))
 
-  (define (execute-until-start-of-cycle instruction-cycle-map registers cycle-count)
-    (execute-for-cycles instruction-cycle-map registers (max 0 (- cycle-count 1))))
+  (define (execute-for-cycles instruction-cycle-map registers cycle-count cycle-total screen)
+      (if (or (= (length instruction-cycle-map) 0) (= cycle-count 0))
+          (list instruction-cycle-map registers screen)
+          (let* ([instruction-cycle-item (car instruction-cycle-map)]
+                 [remaining-inst-cycles (vector-ref instruction-cycle-item 0)]
+                 [instruction (vector-ref instruction-cycle-item 1)]
+                 [new-screen (update-screen screen 0 (- cycle-total cycle-count) registers)])
+            (if (> remaining-inst-cycles 1)
+                (execute-for-cycles
+                 (cons
+                  (vector (- remaining-inst-cycles 1) instruction)
+                  (cdr instruction-cycle-map))
+                 registers
+                 (- cycle-count 1)
+                 cycle-total
+                 new-screen)
+                (execute-for-cycles
+                 (cdr instruction-cycle-map)
+                 (execute-instruction instruction registers)
+                 (- cycle-count 1)
+                 cycle-total
+                 new-screen)))))
 
-  (define (run-script filename)
-    (let* ([cpu-instrs (open-cpu-instructions-file filename)]
-           [instr-cycle-map (map (lambda (v) (vector (cycle-count (car v)) v)) cpu-instrs)]
-           [signal-strengths
-            (map
-             (lambda (cycle) (* cycle (regs-x (cdr (execute-until-start-of-cycle instr-cycle-map (regs 1) cycle)))))
-             '(20 60 100 140 180 220))])
-      (printf "Signal strengths are ~A, sum is ~A\n" signal-strengths (apply + signal-strengths)))))
+    (define (execute-until-start-of-cycle instruction-cycle-map registers cycle-count screen)
+      (execute-for-cycles instruction-cycle-map registers (max 0 (- cycle-count 1))                          
+                          (max 0 (- cycle-count 1)) screen))
+
+    (define (run-script filename)
+      (let* ([cpu-instrs (open-cpu-instructions-file filename)]
+             [instr-cycle-map (map (lambda (v) (vector (cycle-count (car v)) v)) cpu-instrs)]
+             [signal-strengths
+              (map
+               (lambda (cycle) (* cycle
+                                  (regs-x (second (execute-until-start-of-cycle instr-cycle-map (regs 1) cycle #f)))))
+               '(20 60 100 140 180 220))])
+        (printf "Signal strengths are ~A, sum is ~A\n" signal-strengths (apply + signal-strengths))))
+
+    (define (create-screen)
+      (vector->immutable-vector (make-vector 240 #f)))
+
+
+    (define (screen-line->string screen-line)
+      (list->string (vector->list screen-line)))
+    
+    (define (create-screen-lines screen)
+      (if (<= (vector-length screen) 40)
+          (list (screen-line->string screen))
+          (let-values ([(screen-line screen-remain)
+                        (vector-split-at screen 40)])
+            (cons (screen-line->string screen-line)                  
+                  (create-screen-lines screen-remain)))))
+    
+    (define (draw-screen screen)
+      (let ([screen-data (vector-map (lambda (pixel)                                
+                                (if pixel #\# #\.)) screen)])
+        (string-join (create-screen-lines screen-data) "\n")))
+
+    
+    (define (run-script-2 filename)
+      (let* ([cpu-instrs (open-cpu-instructions-file filename)]
+             [instr-cycle-map (map (lambda (v) (vector (cycle-count (car v)) v)) cpu-instrs)]
+             [screen (create-screen)]
+             [data (execute-until-start-of-cycle instr-cycle-map (regs 1) 241 screen)]
+             [screen-contents (draw-screen (third data))])
+        (printf "Screen content:\n~A\n" screen-contents))))
