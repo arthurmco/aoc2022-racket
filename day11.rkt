7ffb6eb70ce89ad9dee79d8662d4ef5e0e8b21e5
Arthur Mendes <arthurmco@gmail.com>
2022-12-12 03:13:30 -0300

    day 11 part 2 using a better solution

--- day11.rkt
+++ day11.rkt
@@ -18,7 +18,8 @@
     (id items operation test
         throw-at-monkey-true
         throw-at-monkey-false
-        inspection-count)
+        inspection-count
+        divisible-value)
     #:transparent)
 
   (define (parse-monkey-id monkey)
@@ -41,7 +42,7 @@
       [(list op "old" num) (lambda (x) (op x (string->number num)))]
       [(list op num "old") (lambda (x) (op (string->number num) x))]))
 
-  
+
   (define (parse-monkey-operation monkey)
     (let ([opstr (map string-trim (string-split (third monkey) ":"))])
       (if (equal? (car opstr) "Operation")
@@ -70,6 +71,18 @@
                 #f))
           #f)))
 
+  (define (get-monkey-divisible-value monkey)
+    (let ([opstr (map string-trim (string-split (fourth monkey) ":"))])
+      (if (equal? (car opstr) "Test")
+          (let* ([terms (map string-trim (string-split (second opstr) "by"))]
+                 [operation (car terms)]
+                 [number (string->number (second terms))])
+            (if (equal? operation "divisible")
+                number
+                #f))
+          #f)))
+
+
   (define (parse-monkey-test-results monkey)
     (let ([monkey-true (string->number (substring (fifth monkey) 29))]
           [monkey-false (string->number (substring (sixth monkey) 30))])
@@ -80,13 +93,14 @@
     (let ([id (parse-monkey-id slist)]
           [starting-items (parse-monkey-starting-items slist)]
           [operation (parse-monkey-operation slist)]
+          [divisible-value (get-monkey-divisible-value slist)]
           [test (parse-monkey-divisible-test slist)]
           [test-results (parse-monkey-test-results slist)])
       (if (and id starting-items operation test test-results)
-          (monkey id starting-items operation test (car test-results) (cdr test-results) 0)
+          (monkey id starting-items operation test (car test-results) (cdr test-results) 0 divisible-value)
           (error "Invalid monkey data"))))
 
-  
+
   (define (parse-monkey-info monkey-def-file-data)
     (if (null? monkey-def-file-data)
         '()
@@ -101,9 +115,9 @@
 
   (define (get-item-worry-levels monkey given-items relief-fn)
     (map (compose relief-fn
-                  (monkey-operation monkey))         
+                  (monkey-operation monkey))
          (append (monkey-items monkey) given-items)))
-  
+
 
   (define (update-monkey-data monkey-data worry-levels interactions)
     (monkey
@@ -115,59 +129,61 @@
      (monkey-throw-at-monkey-false monkey-data)
      (+ (monkey-inspection-count monkey-data)
         (length (monkey-items monkey-data))
-        interactions )))
-  
+        interactions)
+     (monkey-divisible-value monkey-data)))
+
   (define (update-monkey-given-items gihash worry-levels m)
     (let ([worry-true (filter (monkey-test m) worry-levels)]
           [worry-false (filter-not (monkey-test m) worry-levels)])
       (hash-update
        (hash-update gihash (monkey-throw-at-monkey-true m) (curry append worry-true) '())
        (monkey-throw-at-monkey-false m) (curry append worry-false) '())))
-  
-  (define (iterate-monkey-list monkey-list relief-fn)
-    (let ([new-worry-levels (foldl
-             (lambda (monkey-data mstate)
-               (let* ([given-items (hash-ref (car mstate) (monkey-id monkey-data) '())]
-                      [worry-levels (get-item-worry-levels
-                                     monkey-data given-items
-                                     relief-fn)]
-                      [items (update-monkey-given-items
-                              (hash-set (car mstate) (monkey-id monkey-data) '())
-                              worry-levels
-                              monkey-data)])
-                 (cons
-                  items
-                  (hash-set (cdr mstate) (monkey-id monkey-data)
-                            (length (hash-ref (car mstate) (monkey-id monkey-data) '()))))))
-             (cons (make-immutable-hasheq) (make-immutable-hasheq))
-             monkey-list)])
-      (map (lambda (m)
-             (update-monkey-data
-              m
-              (hash-ref (car new-worry-levels) (monkey-id m) '())
-              (hash-ref (cdr new-worry-levels) (monkey-id m) '())))
-           monkey-list)))
 
-  (define (iterate-monkey-list-n-times monkey-list n relief-fn)
-    (when (= (remainder n 20) 0)
-      (display n)
-      (display "\n"))
+  (define (less-common-multiplier monkey-vec)
+    (apply * (vector->list (vector-map monkey-divisible-value monkey-vec))))
+
+  (define (iterate-monkey-list monkey-vec relief-fn)
+    (let ([new-worry-levels (foldl
+                             (lambda (monkey-data mstate)
+                               (let* ([given-items (hash-ref (car mstate) (monkey-id monkey-data) '())]
+                                      [worry-levels (get-item-worry-levels
+                                                     monkey-data given-items
+                                                     relief-fn)]
+                                      [items (update-monkey-given-items
+                                              (hash-set (car mstate) (monkey-id monkey-data) '())
+                                              worry-levels
+                                              monkey-data)])
+                                 (cons
+                                  items
+                                  (hash-set (cdr mstate) (monkey-id monkey-data)
+                                            (length (hash-ref (car mstate) (monkey-id monkey-data) '()))))))
+                             (cons (make-immutable-hasheq) (make-immutable-hasheq))
+                             (vector->list monkey-vec))])
+      (vector-map (lambda (m)
+                    (update-monkey-data
+                     m
+                     (hash-ref (car new-worry-levels) (monkey-id m) '())
+                     (hash-ref (cdr new-worry-levels) (monkey-id m) '())))
+                  monkey-vec)))
+
+  (define (iterate-monkey-list-n-times monkey-vec n relief-fn)
     (if (<= n 0)
-        monkey-list
-        (iterate-monkey-list-n-times (iterate-monkey-list monkey-list relief-fn) (- n 1) relief-fn)))
+        monkey-vec
+        (iterate-monkey-list-n-times (iterate-monkey-list monkey-vec relief-fn) (- n 1) relief-fn)))
 
   (define (get-monkey-business monkey-list)
-    (apply * (take (sort (map monkey-inspection-count monkey-list) >) 2)))
-  
-  
+    (apply * (take (sort (vector->list (vector-map monkey-inspection-count monkey-list)) >) 2)))
+
+
   (define (run-script filename)
-    (let* ([monkey-list (parse-monkey-info (open-monkey-def-file filename))]
+    (let* ([monkey-list (list->vector (parse-monkey-info (open-monkey-def-file filename)))]
            [monkey-after (iterate-monkey-list-n-times monkey-list 20 normal-relief)])
       (printf "Monkey business total: ~A\n" (get-monkey-business monkey-after))))
 
   (define (run-script-2 filename)
-    (let* ([monkey-list (parse-monkey-info (open-monkey-def-file filename))]
-           [monkey-after (iterate-monkey-list-n-times monkey-list 10000 identity)])
+    (let* ([monkey-list (list->vector (parse-monkey-info (open-monkey-def-file filename)))]
+           [multiplier (less-common-multiplier monkey-list)]
+           [monkey-after (iterate-monkey-list-n-times monkey-list 10000 (lambda (v) (remainder v multiplier)))])
       (printf "Monkey business total (with extra worry): ~A\n" (get-monkey-business monkey-after))))
 
   )
